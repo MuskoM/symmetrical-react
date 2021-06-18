c7f60bbdb68f30da234ea9854f865e751d3e592e
Mateusz Musko <muskomatt@outlook.com>
2021-06-18 12:03:46 +0200

    Menu Items

---
+++ src/components/home-page/home-page.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home-page.styles.scss'
+import Directory from '../directory/directory.component'
+
+
+export const HomePage = () =>{
+
+    return <div className="homepage">
+       <Directory/>
+    </div>
+
+}
